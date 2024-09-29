f52e4e6ddbc83c17b0ea06bbe9caa1450b11c89c
RafalFak <rafalfak8@gmail.com>
2024-09-29 15:21:09 +0200

    feat: focusCardArea on name and date

--- js/modules/custom.js
+++ js/modules/custom.js
@@ -1,8 +1,15 @@
 export default function () {
   const card = document.querySelector('#card');
   const cardFocusFrame = document.querySelector('#card__focus-frame');
+
   const numberInput = document.querySelector('#number-input');
+  const nameInput = document.querySelector('#name-input');
+  const dateMonthSelect = document.querySelector('#date-month-select');
+  const dateYearSelect = document.querySelector('#date-year-select');
+
   const numberCard = document.querySelector('#number-card');
+  const nameCard = document.querySelector('#name-card');
+  const dateCard = document.querySelector('#date-card');
 
   const focusCardArea = (cardArea) => {
     const cardAreaRect = cardArea.getBoundingClientRect();
@@ -26,6 +33,20 @@ export default function () {
   numberInput.addEventListener('focus', () => {
     focusCardArea(numberCard);
   });
-
   numberInput.addEventListener('blur', unfocusCardArea);
+
+  nameInput.addEventListener('focus', () => {
+    focusCardArea(nameCard);
+  });
+  nameInput.addEventListener('blur', unfocusCardArea);
+
+  dateMonthSelect.addEventListener('focus', () => {
+    focusCardArea(dateCard);
+  });
+  dateMonthSelect.addEventListener('blur', unfocusCardArea);
+
+  dateYearSelect.addEventListener('focus', () => {
+    focusCardArea(dateCard);
+  });
+  dateYearSelect.addEventListener('blur', unfocusCardArea);
 }
